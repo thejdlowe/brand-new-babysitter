dba5f8974b0931a47ba77fce8c60689294125c9f
J.D. Lowe <jdlowe.dev@gmail.com>
2023-08-18 19:34:44 -0400

    Added context!

--- src/components/BabySitterContext/BabySitterContext.tsx
+++ src/components/BabySitterContext/BabySitterContext.tsx
@@ -1,1 +1,21 @@
-export const BabySitterContext = () => {}
+import React, {createContext, useContext, useState} from "react";
+export interface BabySitterContextProviderProps {
+    children: React.ReactChild;
+    anyAdditionalFunctions: () => void | undefined;
+}
+interface BabySitterContextValue {
+
+};
+
+const BabySitterContext = createContext<BabySitterContextValue>({});
+export const BabySitterContextProvider: React.FC<BabySitterContextProviderProps> = ({children, anyAdditionalFunctions}) => {
+    return (<BabySitterContext.Provider value={{}}>
+        {children}
+    </BabySitterContext.Provider>)
+}
+
+export const useBabySitterContext = () => {
+    const value = useContext(BabySitterContext);
+    return value;
+}
+//export const BabySitterContext = () => {}
