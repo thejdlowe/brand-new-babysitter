387eb8412f60abd2fef749f8f845624fcbb9a4ed
J.D. Lowe <jdlowe.dev@gmail.com>
2023-08-22 17:32:26 -0400

    Moved delete button, adding add button (NOT WORKING)

--- src/components/Body/components/PlayerList/PlayerList.tsx
+++ src/components/Body/components/PlayerList/PlayerList.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState } from "react";
 import { Grid, FormControlLabel, Switch, IconButton } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 import { useBabySitterContext } from "../../../BabySitterContext";
 
@@ -44,25 +45,37 @@ export const PlayerList: FC = () => {
 				/>
 				<IconButton onClick={() => {
 					deletePlayer(name);
-				}} className="hidden-button">
+				}}>
 					<DeleteIcon />
 				</IconButton>
 			</Grid>
 		);
 	};
 	return (
+		<>
 		<Grid container>
 			{playerNames.map((playerName) => {
 				return <>{buildPlayer(playerName, players[playerName])}</>;
 			})}
-			<Grid item xs={3}>
+			
+		</Grid>
+
+		<Grid item xs={3}>
 				Delete All Players
 				<IconButton onClick={() => {
 					deletePlayer();
-				}}  className="hidden-button">
+				}}>
 					<DeleteIcon />
 				</IconButton>
 			</Grid>
-		</Grid>
+			<Grid item xs={3}>
+				Add New Player
+				<IconButton onClick={() => {
+					
+				}}>
+					<AddCircleIcon />
+				</IconButton>
+			</Grid>
+		</>
 	);
 };
