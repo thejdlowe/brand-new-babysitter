7a48f59d5cfe6ff6658e1f2b44133e53b4116e4c
J.D. Lowe <jdlowe.dev@gmail.com>
2023-08-29 14:04:18 -0400

    server now has text moderation

--- api/index.ts
+++ api/index.ts
@@ -3,11 +3,41 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// this example uses axios and form-data
+const axios = require("axios");
+const FormData = require("form-data");
+
 const app: Express = express();
 const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Express + TypeScript Server");
+});
+
+app.get("/text", (req: Request, res: Response) => {
+	const data = new FormData();
+	data.append("text", "You gotta stop farting so much");
+	data.append("lang", "en");
+	data.append("mode", "ml");
+	data.append("api_user", `${process.env.SIGHTENGINE_USER}`);
+	data.append("api_secret", `${process.env.SIGHTENGINE_SECRET}`);
+
+	axios({
+		url: "https://api.sightengine.com/1.0/text/check.json",
+		method: "post",
+		data: data,
+		headers: data.getHeaders(),
+	})
+		.then(function (response: any) {
+			// on success: handle response
+			//console.log(response.data);
+			res.send(response.data);
+		})
+		.catch(function (error: any) {
+			// handle error
+			if (error.response) console.log(error.response.data);
+			else console.log(error.message);
+		});
 });
 
 app.listen(port, () => {
