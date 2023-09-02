e8f1d43ba4699bdae15857adbbc286142a03adbb
J.D. Lowe <jdlowe.dev@gmail.com>
2023-09-01 21:54:50 -0400

    Adding suggestion database

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,15 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
 
+const sqlite3 = require("sqlite3").verbose();
+
+let db = new sqlite3.Database("./db.sqlite");
+
+db.exec(`CREATE TABLE IF NOT EXISTS suggestions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    suggestion VARCHAR(100) NOT NULL
+)`);
+
 dotenv.config();
 
 // this example uses axios and form-data
@@ -55,8 +64,18 @@ app.post("/suggestion", (req: Request, res: Response) => {
 						}
 					});
 					if (good === true) {
-						suggestions.push(suggestion);
-						res.send(response.data);
+						db.run(
+							`INSERT INTO suggestions(suggestion) VALUES(?)`,
+							[suggestion],
+							(err: any) => {
+								if (err) {
+									console.log(err);
+									throw { message: "Unable to add suggestion to database" };
+								}
+								res.send(response.data);
+								return;
+							}
+						);
 						return;
 					}
 					throw { message: message.join(", ") };
@@ -75,8 +94,27 @@ app.post("/suggestion", (req: Request, res: Response) => {
 });
 
 app.get("/suggestion", (req: Request, res: Response) => {
-	console.log({ suggestions });
-	return res.json({ suggestions });
+	db.get(
+		`SELECT * FROM suggestions ORDER BY RANDOM() LIMIT 1;`,
+		[],
+		(err: any, row: any) => {
+			if (err) {
+				res.status(500).send("Unable to select a suggestion");
+			}
+			if (!row) return res.json({ suggestion: null });
+			db.get(`delete from suggestions where id = ?`, [row.id], () => {});
+			return res.json({ suggestion: row.suggestion });
+		}
+	);
+});
+
+app.get("/reset", (req: Request, res: Response) => {
+	db.get(`delete from suggestions`, [], (err: any, row: any) => {
+		if (err) {
+			res.status(500).json({ resetSuccessfully: false });
+		}
+		return res.json({ resetSuccessfully: true });
+	});
 });
 
 app.listen(port, () => {
